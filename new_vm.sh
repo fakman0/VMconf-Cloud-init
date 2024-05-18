f72f3f6a4c5925e3b57f83043a430b4ee82a0e03
Furkan <81708922+fakman0@users.noreply.github.com>
2024-05-18 21:22:23 +0300

    Update new_vm.sh

--- new_vm.sh
+++ new_vm.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-read -p "ssh port (leave blank for default "22"): " tempvar_sshport
-read -p "hostname (leave blank for default $(hostname)): " tempvar_hostname
+read -p "Ssh port (leave blank for default 22): " tempvar_sshport
+read -p "Hostname (leave blank for default $(hostname)): " tempvar_hostname
+read -p "Reboot after done Y/N (leave blank for default Y): " tempvar_reboot
 ipaddr=$(hostname -I | awk '{print $1}')
 
 apt update && apt upgrade -y
 apt install git wget curl vim nano systemd-timesyncd -y
 
-if [ $tempvar_hostname ]; then
+if [ -n "$tempvar_hostname" ]; then
   hostnamectl set-hostname $tempvar_hostname
 fi
 
-if [ $tempvar_sshport]; then
+if [ -n "$tempvar_sshport" ]; then
   sed -i "s/.*Port .*/Port $tempvar_sshport/" "/etc/ssh/sshd_config"
   systemctl restart sshd
 fi
 
-if grep -q $ipaddr /etc/hosts ; then
+if grep -q "$ipaddr" /etc/hosts ; then
   echo "There is a different definition in /etc/hosts"
 else
-  sed -i '2 a\'"$ipaddr   $tempvar_hostname" /etc/hosts
+
+  sed -i '2 a\\'"$ipaddr   $tempvar_hostname" /etc/hosts
 fi
 
 timedatectl set-timezone "Europe/Istanbul"
@@ -32,4 +34,8 @@ echo 'export LANG="en_US.UTF-8"' >> /root/.bashrc
 echo 'export LC_ALL="en_US.UTF-8"' >> /root/.bashrc
 echo 'export LC_CTYPE="en_US.UTF-8"' >> /root/.bashrc
 
+if [ "$tempvar_reboot" == Y ]; then
+  reboot
+fi
 
+rm -- "$0"
