1699f035f6f146b67e9c7df6e141d915fa0fa837
Furkan <81708922+fakman0@users.noreply.github.com>
2024-05-19 14:46:57 +0300

    Update new_vm.sh

--- new_vm.sh
+++ new_vm.sh
@@ -8,12 +8,11 @@ echo 'export LC_ALL="en_US.UTF-8"' >> /root/.bashrc
 echo 'export LC_CTYPE="en_US.UTF-8"' >> /root/.bashrc
 
 read -p "Ssh Port (leave blank for default 22): " tempvar_sshport
-tempvar_sshpassauth=N
 read -p "Ssh PasswordAuthentication Close:N/Open:Y (leave blank for default N):" tempvar_sshpassauth
-if [ "$tempvar_sshpassauth" == N ]; then
-  if [ -s "~/.ssh/authorized_keys" ]; then
-  echo "There is no authorized ssh-key registered to the user, if you continue you may lose your ssh access."
-  read -p "Please enter ssh-key (leave blank to continue): " tempvar_sshkey
+if [ "$tempvar_sshpassauth" == N ] || [ -z "$tempvar_sshpassauth" ]; then
+  if [ -z "$(cat ~/.ssh/authorized_keys)" ]; then
+    echo "There is no authorized ssh-key registered to the user, if you continue you may lose your ssh access."
+    read -p "Please enter ssh-key (leave blank to continue): " tempvar_sshkey
     if [ -n "$tempvar_sshkey" ]; then
       echo "$tempvar_sshkey" >> ~/.ssh/authorized_keys
     fi
